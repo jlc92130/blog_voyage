5b454584a8aa58b1cfa019ecc6483a5fb88165e3
jlc92130 <jlc92130@gmail.com>
2022-03-23 11:32:12 +0100

    add footer and css classes

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,18 @@
+// Librairies
+import React, { useState } from 'react';
+import classes from './Footer.module.css';
+
+
+ 
+
+function Footer() {
+    return (
+        <footer className={classes.Footer}>
+            <div>
+                2022 © Blog
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -3,15 +3,21 @@ import React from 'react';
 
 // Composants
 import Header from '../../Components/Header/Header';
+import Footer from '../../Components/Footer/Footer';
+import classes from './Layout.module.css'
 
 function Layout(props) {
   return (
-      <>  
+      <div className={classes.Layout}>  
         {/* En tête  */}
         <Header />
-        {props.children}
+        {/* Corps  */}
+        <div className={classes.content}>
+          {props.children}
+        </div>
         {/* Pied de page */}
-      </>  
+        <Footer />
+      </div>  
   );
 }
 
